b9ba34e45f1d11ebdcae69b88a8ec92eecd6ec6c
bdx_inc <bdxinc.contact@gmail.com>
2017-12-14 01:08:08 -0800

    removed stupid stuff
    removed stupid stuff

--- bdx_simple_calculator.js
+++ bdx_simple_calculator.js
@@ -13,41 +13,21 @@
   buttonsEl.addEventListener("click", handleClick, true);
 
   var subTotal = 0;
-  var operator;
+  // var operator;
   var number = 0;
-  // var total = subTotal operator number;
 
   function buttonType(buttonContent){
     var numbers = /[0-9]+/g;
     var decimal = /[.]+/gi;
-    var mathOperator = /[+-÷×=]+/g;
+    var mathOperator = /[+-÷×*/=]+/g;
     var clear = /[AC/C]+/g;
 
 
     if(buttonContent.match(numbers)) {
-      console.log(buttonContent);
-      // stuff should happen...
       var num = parseInt(buttonContent);
-      // document.getElementById("total").textContent = number;
       buildNumber(num);
-      // numStr.push(buttonContent);
-      // document.getElementById("total").textContent = numStr.join("");
-
-    } else if(buttonContent.match(decimal)) {
-      console.log(buttonContent);
-      // stuff should happen...
-      // decimal(buttonContent);
-
     } else if(buttonContent.match(mathOperator)) {
-      console.log(buttonContent);
-      // stuff should happen...
-      mathOperation(buttonContent);
-
-    } else if(buttonContent.match(clear)) {
-      console.log(buttonContent);
-      // stuff should happen...
-      // clear(buttonContent);
-
+      equals(buttonContent);
     }
   }
 
@@ -55,42 +35,11 @@
   function buildNumber(num) {
     number = (number * 10) + num;
     document.getElementById("total").textContent = number;
-    equals(2, 1, "+");
   }
 
 
-
-
-  function mathOperation(buttonContent) {
-      switch(buttonContent) {
-        case "+":
-          equals("+");
-          break;
-        case "-":
-          equals("-");
-          break;
-        case "×":
-          equals("×");
-          break;
-        case "÷":
-          if(number != 0) {
-            equals("÷");
-          } else {
-            console.log("dividing by zero");
-          }
-          break;
-        case "=":
-        equals("=");
-          break;
-      }
-      document.getElementById("total").textContent = subTotal;
-      console.log(subTotal);
-      number = 0;
-  }
-
-
-  function equals(subTotal, number, operator) {
-    switch(operator) {
+  function equals(buttonContent) {
+    switch(buttonContent) {
       case "+":
         subTotal += number;
         break;
@@ -98,9 +47,11 @@
         subTotal -= number;
         break;
       case "×":
+      case "*":
         subTotal *= number;
         break;
       case "÷":
+      case "/":
         subTotal /= number;
         break;
     }
@@ -110,22 +61,28 @@
   }
 
 
-
-  function decimals(buttonContent) {
-      // stuff
-  }
-
-  function clear(buttonContent) {
-      // stuff
-  }
-
-
 })(window);
 
 
 
 
 // ** NONSENSE (TO-DO LIST:) **
+// [0-9]/[+-*/=]/[0-9]/[+-*/=]......
+// var leftOp = ;
+//
+// var operator= ;
+//
+// var rightOp =;
+//
+//
+//
+// leftOp() {}
+//
+// operator() {}
+//
+// rightOp() {}
+//
+// number +-*/ number
 
 
 // eventListener...
