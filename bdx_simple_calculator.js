71d5fcbfaca5c921da07cf121c44482f0f6e0a41
bdx_inc <bdxinc.contact@gmail.com>
2017-12-17 02:47:05 -0800

    started over - much better now
    started over - much better now

--- bdx_simple_calculator.js
+++ bdx_simple_calculator.js
@@ -1,242 +1,135 @@
-/*
 (function(window) {
+
+  const calculator = (function(){
+    const MATH_OPERATORS = "+*-/";
+    const INITIAL_MEMORY = {
+      total: '0',
+      log: '',
+      decimal: false,
+      reset: false
+    };
+
+
+    let memory = Object.assign({}, INITIAL_MEMORY);
+
+
+    const screen = (function() {
+      const log = document.getElementById("log");
+      const total = document.getElementById("total");
+
+
+      function update(data) {
+        let newTotal = '';
+        let newLog = '';
+
+        if(/*and stuff*/) {
+          // things should happen
+        }
+      }
+
+
+      function clear() {
+        memory = Object.assign({}, INITIAL_MEMORY);
+        update({total: memory.total, log: memory.log});
+      }
+
+      return {
+        clear: clear,
+        update: update
+      }
+    })();
+
+
+    function isLogEmpty() {
+      return memory.log.length === 0;
+    }
+
+
+    function containsOperator(str) {
+      return OPERATORS.indexOf(str) !== -1;
+    }
+
+
+    function parseCalc(str) {
+      let equation = [];
+      let current = "";
+      // stuff goes here
+
+      for(/*loop hell*/) {
+        if(/*nonsense*/) {
+          // more stuff
+        }
+      }
+    }
+
+
+    function equals(calc) {
+      // stuff goes here
+      const operator = [
+        {
+          "*": function(a,b) {return a * b},
+          "/": function(a,b) {return a / b}
+        },
+        {
+          "+": function(a,b) {return a + b},
+          "-": function(a,b) {return a - b}
+        }
+      ]
+
+      let newCalc = [];
+      let currentOp;
+
+      for(/*loop hell*/) {
+        if(/*nonsense*/) {
+          // more stuff
+        }
+      }
+
+      calc = newCalc;
+      newCalc = [];
+
+      if(/*nonsense*/) {
+        // more stuff
+      }
+    }
+
+
+    function isCalcValueValid(value) {
+      // stuff goes here
+      const logLength = memory.log.length;
+
+      if(/*nonsense*/) {
+        // more stuff
+      }
+    }
+
+
+    function append(value) {
+      // stuff goes here
+      if(/*nonsense*/) {
+        // more stuff
+      }
+    }
+
+    return {
+      input: append,
+      clear: screen.clear
+    }
+
+  })();
+
 
   function handleClick(event) {
     if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
-      console.log(event.target.textContent);
+      console.log(event.target.value || event.target.textContent);
 
-      buttonType(event.target.textContent);
+      calculator.input(event.target.value || event.target.textContent);
     }
     event.stopPropagation();
   }
 
-  var buttonsEl = document.getElementById("main");
+  const buttonsEl = document.getElementById("main");
   buttonsEl.addEventListener("click", handleClick, true);
 
-  var subTotal = 0;
-  // var operator;
-  var number = 0;
-
-  function buttonType(buttonContent){
-    var numbers = /[0-9]+/g;
-    var decimal = /[.]+/gi;
-    var mathOperator = /[+-÷/×*=]+/g;
-    var clear = /[AC/C]+/g;
-
-
-    if(buttonContent.match(numbers)) {
-      var num = parseInt(buttonContent);
-      buildNumber(num);
-    } else if(buttonContent.match(mathOperator)) {
-      equals(buttonContent);
-    }
-  }
-
-
-  function buildNumber(num) {
-    number = (number * 10) + num;
-    document.getElementById("total").textContent = number;
-  }
-
-
-  function equals(buttonContent) {
-    switch(buttonContent) {
-      case "+":
-        subTotal += number;
-        break;
-      case "-":
-        subTotal -= number;
-        break;
-      case "×":
-      case "*":
-        subTotal *= number;
-        break;
-      case "÷":
-      case "/":
-        subTotal /= number;
-        break;
-    }
-    document.getElementById("total").textContent = subTotal;
-    console.log(subTotal);
-    number = 0;
-  }
-
 
 })(window);
-
-*/
-
-
-
-
-
-
-
-
-// ** NONSENSE (TO-DO LIST:) **
-
-(function(window) {
-
-//receive operands
-
-function handleClick(event) {
-  if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
-    console.log(event.target.textContent);
-
-    screen(event.target.textContent);
-  }
-  event.stopPropagation();
-}
-
-var buttonsEl = document.getElementById("main");
-buttonsEl.addEventListener("click", handleClick, true);
-
-
-
-//calculate
-
-
-
-
-//screen
-
-var equationLog = [];
-
-function screen(button) {
-  equationLog.push(button);
-  document.getElementById("total").textContent = equationLog.join('');
-}
-
-
-
-
-
-//clear
-
-
-
-})(window);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// [0-9]/[+/-*=]/[0-9]/[+/-*=]......
-// var leftOp = ;
-//
-// var operator= ;
-//
-// var rightOp =;
-//
-//
-//
-// leftOp() {}
-//
-// operator() {}
-//
-// rightOp() {}
-//
-// number +*-/ number
-
-
-// eventListener...
-//
-// subTotal()
-//
-// number()
-//
-// c(a,b,c)
-//
-// switch(c)
-//   total = subTotal + number;
-//
-//
-//
-//
-
-
-
-
-
-
-
-// function target(DomElement){
-//   DomElement=document.getElementById(DomElement);
-//   return DomElement;
-// }
-
-
-
-//
-// type in a  #
-// var numOne = #;
-//
-// press an operator
-// var currentOperator = +*-/...;
-//
-// key in another ##
-// var numTwo = ##;
-//
-// then we press equals/=;
-// runs the switch case currentOperator(buttonContent)
-
-//
-// user clicks calc number
-// // gets stored the number in an array (numStr)
-// // display the number with a numStr.join
-//
-//
-//
-//
-// then they click math mathOperation
-//
-//
-// then select another numbers
-//
-// then select equal (math operator)
-//
-// returns total
-//
-//
-//
-//
-// store it in an array - numString
-//
-// add / concat... buttonContent
-//
-// display/return numString
